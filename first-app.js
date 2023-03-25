5bbc39c6d3b9b31b6b5e9382328cc049b8aa77e9
AbhayBarolia <abhay28barolia@gmail.com>
2023-03-26 03:21:09 +0530

    New server created

--- first-app.js
+++ first-app.js
@@ -1,3 +1,26 @@
-const fs= require('fs');
-fs.writeFileSync('hello.txt','Hello world');
-console.log('Hello world');
+/* let arr= ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+console.log(arr.map(fruit=>{
+    if(fruit===' ')
+    return 'empty string';
+    else
+    return fruit;
+})); 
+
+console.log('a');
+
+console.log('b');
+
+let promise = new Promise((res,rej)=>{
+    res(setTimeout(console.log('c'), 3000));
+});
+let promise1 = new Promise((res,rej)=>{
+    res(setTimeout(console.log('d'), 0));
+});
+//promise.then(promise1);
+console.log('e'); */
+const http= require('http');
+const server = http.createServer((req,res)=>
+{console.log('Server created by Abhay');}
+);
+server.listen(4000);
+
